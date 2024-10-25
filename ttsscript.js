f0de31e4d4a352e471a73107d9dded84e8c95d4d
Quang Nguyen <dqvn2002@gmail.com>
2024-10-25 09:05:10 +0200

    update

--- ttsscript.js
+++ ttsscript.js
@@ -4,7 +4,7 @@ const TTSName = 'Google Nederlands';
 const TTSLang = 'nl-NL';
 
 // assume you have an array of filenames
-const fileNames = ["ch03", "file2"];
+const fileNames = ["ch01","ch03"];
 
 const speech = new SpeechSynthesisUtterance();
 speech.lang = TTSLang;
