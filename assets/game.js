64f40ac72d90872ee781101639348190eeb265c4
Quang Nguyen <dqvn2002@gmail.com>
2024-11-01 10:48:01 +0100

    update

--- assets/game.js
+++ assets/game.js
@@ -19,7 +19,7 @@ function showQuestion() {
         const currentWord = data[currentWordIndex];
 
         const options = [currentWord.english];
-        while (options.length < 5) {
+        while (options.length < 10) {
             const randomIndex = Math.floor(Math.random() * data.length);
             const randomOption = data[randomIndex].english;
             if (!options.includes(randomOption)) {
