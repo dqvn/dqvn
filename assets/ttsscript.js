5ad20dc37dc3905da04fe15de6a2af8fd36d410e
Quang Nguyen <dqvn2002@gmail.com>
2024-12-14 09:46:43 +0100

    update

--- assets/ttsscript.js
+++ assets/ttsscript.js
@@ -3,7 +3,7 @@ const TTSLang = 'nl-NL';
 const TTSLangENG = 'en-US';
 const fileNames = ["ch00", "ch01", "ch03", "ch02", "ch04", "ch05", "ch06", "ch07", "ch08", "ch09", "ch10", "sp02", "sp03",
   "sp04", "sp05", "sp06", "sp07", "sp08", "sp09", "sp10", "sp11", "sp12", "sp13", "sp14", "sp15", "sp16", "sp17", "sp18",
-  "sp19", "sp20", "sp21", "sp22", "sp23", "sp24", "sp25", "sp26", "sp27", "sw2", "sw5", "sw7", "sw9", "sw10", "sw12", "sw13",
+  "sp19", "sp20", "sp21", "sp22", "sp23", "sp24", "sp25", "sp26", "sp27", "sw02", "sw05", "sw07", "sw09", "sw10", "sw12", "sw13",
   "sw14", "sw15", "sw16", "sw17", "sw18", "sw19", "sw20", "sw21", "sw22", "sw23", "sw24", "sw25", "sw26", "sw27", "sw28", "sw29", "sw30",
   "sw31", "sw32", "sw33", "sw34", "sw35", "sw36", "sw37", "sw38", "sw39", "sw40", 
   "sw41", "sw42", "sw43", "sw44", "sw45", "sw46", "sw47", "sw48", "sw49", "sw50", 
