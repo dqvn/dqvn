76593f0ea1b4b8aca4f5a8bc4dae94b6fb965ebe
QNguyen <qnguyen@deheus.com>
2025-10-07 15:29:10 +0200

    add vanstart

--- assets/ttsvanstartscript.js
+++ assets/ttsvanstartscript.js
@@ -1,20 +1,11 @@
 const TTSName = 'Google Nederlands';
 const TTSLang = 'nl-NL';
 const TTSLangENG = 'en-US';
-const fileNames = ["ch00", "ch01", "ch03", "ch02", "ch04", "ch05", "ch06", "ch07", "ch08", "ch09", "ch10", "sp02", "sp03",
-  "sp04", "sp05", "sp06", "sp07", "sp08", "sp09", "sp10", "sp11", "sp12", "sp13", "sp14", "sp15", "sp16", "sp17", "sp18",
-  "sp19", "sp20", "sp21", "sp22", "sp23", "sp24", "sp25", "sp26", "sp27", "sw02", "sw05", "sw07", "sw09", "sw10", "sw12", "sw13",
-  "sw14", "sw15", "sw16", "sw17", "sw18", "sw19", "sw20", "sw21", "sw22", "sw23", "sw24", "sw25", "sw26", "sw27", "sw28", "sw29", "sw30",
-  "sw31", "sw32", "sw33", "sw34", "sw35", "sw36", "sw37", "sw38", "sw39", "sw40", 
-  "sw41", "sw42", "sw43", "sw44", "sw45", "sw46", "sw47", "sw48", "sw49", "sw50", 
-  "sw51", "sw52", "sz02", "sz03", "sz04", "sz05", "sz06", "sz07", "sz08", "sz09", "sz10", "sz11", "sz12", "sz13", "sz14", "sz15", "sz16", "sz17", "sz18", "sz19"];
+const fileNames = ["thema01", "thema02", "thema03", "thema04", "thema05", "thema06", "thema07", "thema08"];
 const INTERVAL_TIME = 8000;
 
 const GROUP_TITLES = new Map([
-  ["ch", "Dutch Class #1"],
-  ["sp", "Learn Dutch #2"],
-  ["sw", "Learn Dutch #3"],
-  ["sz", "Learn Dutch #4"],
+  ["thema", "Van Start #1"]
 ]);
 
 // create a table body element
