1e1f185d21a49afc2eec809c3241cc74766e67f0
QNguyen <qnguyen@deheus.com>
2026-02-25 09:38:36 +0100

    add pronunciation

--- assets/ttsvanstartscript.js
+++ assets/ttsvanstartscript.js
@@ -249,7 +249,7 @@ function reloadTable(jsonData) {
     const row = document.createElement('tr');
     row.innerHTML = `
     <td>${index + 1}</td>
-    <td onclick="speakText('${word.dutch?.replace(/'/g, "")}')"><span class="dutch-word" data-index="${index}">${word.dutch}</span><br/><span style="font-size: 0.7em;">/${word.pronunciation?.ipa || ""}/ <br/> /${word.pronunciation?.phonetic || ""}/</span></td>
+    <td onclick="speakText('${word.dutch?.replace(/'/g, "")}')"><span class="dutch-word" data-index="${index}">${word.dutch}</span> ~ <span style="font-size: 0.7em;">/${word.pronunciation?.ipa || ""}/ <br/> /${word.pronunciation?.phonetic || ""}/</span></td>
     <td onclick="speakEngText('${word.english?.replace(/'/g, "")}')"><span class="hide-text">${word.english}</span></td>
     <td onclick="speakText('${word.dutchsentence?.replace(/'/g, "")}')"><span> ${word.dutchsentence}</span><br/><span class="hide-text" style="color: #3f3838ff; opacity: 0.3;">${word.englishtranslate}</span></td>
     <td><span class="hide-text">${word.vietnamese?.replace(/'/g, "")}</span></td>
